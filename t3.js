48613986267430d1d77921716f98958e1440c80b
Jake <jsmith181997@gmail.com>
2024-03-16 11:31:20 -1000

    Add check for win conditions/game end conditions

--- t3.js
+++ t3.js
@@ -13,6 +13,47 @@ const gameBoard = function () {
 
     const board = [];
 
+    const isGameOver = (symbol) => {
+        const r1 = board.slice(0, 3);
+        const r2 = board.slice(3, 6);
+        const r3 = board.slice(6, 9);
+        const rows = [r1, r2, r3];
+
+        const c1 = [board[0], board[3], board[6]];
+        const c2 = [board[1], board[4], board[7]];
+        const c3 = [board[2], board[5], board[8]];
+        const cols = [c1, c2, c3];
+
+        const d1 = [board[0], board[4], board[8]];
+        const d2 = [board[6], board[4], board[2]];
+        const diags = [d1, d2];
+
+        // check rows
+        for (let row = 0; row < rows.length; row++) {
+            if (row[0] == symbol && row[1] == symbol && row[2] == symbol) {
+                return true;
+            }
+        }
+
+        // check columns
+        for (let col = 0; col < cols.length; col++) {
+            if (col[0] == symbol && col[1] == symbol && col[2] == symbol) {
+                return true;
+            }
+        }
+
+        // check diagnols
+        for (let diag = 0; diag < diags.length; diag++) {
+            if (diag[0] == symbol && diag[1] == symbol && diag[2] == symbol) {
+                return true;
+            }
+        }
+
+        return false;
+
+
+    }
+
     const initBoard = function () {
         for (let i = 0; i < 9; i++) {
             board.push(createSpace());
@@ -25,7 +66,6 @@ const gameBoard = function () {
     const displayBoard = () => {
 
         let row1 = '', row2 = '', row3 = '';
-
         board.forEach((space, index) => {
 
             if (index <= 2) {
@@ -35,9 +75,7 @@ const gameBoard = function () {
             } else {
                 row3 += `${space.getPiece()} `;
             }
-
         })
-
         console.log(row1);
         console.log(row2);
         console.log(row3);
@@ -62,39 +100,32 @@ const gameBoard = function () {
     // Place spaces onto the board, don't need to expose this.
     initBoard();
 
-    return { board, clearBoard, displayBoard };;
+    return { board, clearBoard, displayBoard, isGameOver };;
 
 }();
 
+// Factory pattern for players since we'll need to create multiple.
+function createPlayer(name, symbol) {
+    let score = 0;
 
-gameBoard.displayBoard();
+    const increaseScore = () => score++;
+    const resetScore = () => score = 0;
+    const getScore = () => `${name}'s Score: ${score}`;
 
+    const getName = () => name;
+    const getSymbol = () => symbol;
 
-
-
-
-
+    console.log(`${name} will be ${symbol}`);
+    return { getName, getSymbol, increaseScore, resetScore, getScore, getName };
+}
 
 // Mediator-type module for the game logic
 const playGame = (() => {
 
 
-    // Factory pattern for players since we'll need to create multiple, but inside the module.
-    function createPlayer(name, symbol) {
-        let score = 0;
+    const board = gameBoard;
 
-        const increaseScore = () => score++;
-        const resetScore = () => score = 0;
-        const getScore = () => `${name}'s Score: ${score}`;
-
-        const getName = () => name;
-        const getSymbol = () => symbol;
-
-        console.log(`${name} will be ${symbol}`);
-        return { getName, getSymbol, increaseScore, resetScore, getScore, getName };
-    }
-
-    // wrap up players in an object
+    // Create players and wrap them up nicely in an object
     const players = (() => {
         let player1Name = "player1";
         let player2Name = "player2";
@@ -106,9 +137,65 @@ const playGame = (() => {
     })();
 
 
+    // handle turn
+    function takeTurn(player, target) {
+
+        let picking = true;
+
+        while (picking) {
+            if (!board.board[target].hasPiece()) {
+                board.board[target].placeMarker(player.getSymbol());
+                picking = false;
+
+            } else {
+                console.log("Space already has a piece, choose again!");
+            }
+        }
 
 
 
+        // check if any win conditions exist
 
+    }
+
+    function endGame() {
+        console.log("Game over!");
+        console.log("Winner");
+    }
+
+    function runGame() {
+
+        let playing = true;
+        let turnCount = 1;
+        while (playing) {
+
+            console.log(`Turn: ${turnCount}`);
+            board.displayBoard();
+            console.log('---------------');
+
+            // player1 take turn
+            // check win condition
+            if (board.isGameOver(players.player1.getSymbol())) {
+                playing = false;
+                break;
+
+            }
+            // player2 take turn 
+            // check win condition
+            if (board.isGameOver(players.player2.getSymbol())) {
+                playing = false;
+                break;
+            }
+
+            if (turnCount === 9) {
+                console.log("It's a tie!")
+                playing = false;
+            }
+
+            turnCount++;
+        }
+    }
+
+    runGame();
 
 })();
